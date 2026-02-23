69e79004a6c9e7c6c1f8d1030632aff3c3038e77
Nik <124788493+Dormammu-616@users.noreply.github.com>
2026-02-23 16:06:22 +0300

    Add files via upload

---
+++ Class and object - resource architecture/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "CompetitionManager.h"
+
+int main()
+{
+    std::cout << "Global count: " << Participant::get_live_objects_count() << "\n";
+
+    {
+        std::cout << "\n--- Scope start ---\n";
+        
+        CompetitionManager super_cup{ "Super Cup", 5 };
+        super_cup.dispatch_participant("Alice");
+        super_cup.dispatch_participant("Bob");
+
+        std::cout << "\n--- Stack test ---\n";
+        super_cup.perform_check_up();
+    
+        std::cout << "\n--- Score end ---\n";
+    }
+
+    std::cout << "\n--- End main ---\n";
+    std::cout << "Global count: " << Participant::get_live_objects_count() << "\n";
+
+    return 0;
+}
